ff465db23eacf6afaecaebf50e0aaabb2841d7ee
letelete <brunokawka@gmail.com>
2022-04-21 16:35:42 +0200

    feat: add missing strings

---
+++ packages/theme/components/AppFooter.vue
@@ -0,0 +1,114 @@
+<template>
+  <SfFooter :column="4" multiple class="footer">
+    <SfFooterColumn :title="$t('components.app_footer.about_us.title')">
+      <SfList>
+        <SfListItem
+          v-for="item in $t(`components.app_footer.about_us.items`)"
+          :key="item"
+          >
+          <SfMenuItem
+            :label="item"
+          />
+        </SfListItem>
+      </SfList>
+    </SfFooterColumn>
+    <SfFooterColumn :title="$t('components.app_footer.departments.title')">
+      <SfList>
+        <SfListItem
+          v-for="item in $t(`components.app_footer.departments.items`)"
+          :key="item"
+        >
+          <SfMenuItem
+            :label="$t(item)"
+          />
+        </SfListItem>
+      </SfList>
+    </SfFooterColumn>
+    <SfFooterColumn :title="$t('components.app_footer.help.title')">
+      <SfList>
+        <SfListItem
+          v-for="item in $t(`components.app_footer.help.items`)"
+          :key="item"
+        >
+          <SfMenuItem
+            :label="$t(item)"
+          />
+        </SfListItem>
+      </SfList>
+    </SfFooterColumn>
+    <SfFooterColumn :title="$t('components.app_footer.payment_and_delivery.title')">
+      <SfList>
+        <SfListItem
+          v-for="item in $t(`components.app_footer.payment_and_delivery.items`)"
+          :key="item"
+        >
+          <SfMenuItem
+            :label="$t(item)"
+          />
+        </SfListItem>
+      </SfList>
+    </SfFooterColumn>
+    <SfFooterColumn :title="$t('components.app_footer.social.title')">
+      <div class="footer__socials">
+        <SfImage class="footer__social-image" v-for="item in $t('components.app_footer.social.items')" :key="item" :src="'/icons/'+item+'.svg'" :alt="item" width="32" height="32" />
+      </div>
+    </SfFooterColumn>
+  </SfFooter>
+</template>
+
+<script>
+import { SfFooter, SfList, SfImage, SfMenuItem } from '@storefront-ui/vue';
+
+export default {
+  components: {
+    SfFooter,
+    SfList,
+    SfImage,
+    SfMenuItem
+  },
+  data() {
+    return {
+      isMobile: false,
+      desktopMin: 1024
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+
+.footer {
+  margin-bottom: 3.75rem;
+  @include for-desktop {
+    margin-bottom: 0;
+  }
+  &__socials {
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto var(--spacer-lg);
+    padding: var(--spacer-base) var(--spacer-xl);
+    @include for-desktop {
+      justify-content: flex-start;
+      padding: var(--spacer-xs) 0;
+      margin: 0 auto;
+    }
+  }
+  &__social-image {
+    margin: 0 var(--spacer-2xs) 0 0;
+  }
+}
+.sf-footer {
+  @include for-desktop {
+    border-top: var(--spacer-xs) solid var(--c-primary);
+    padding-bottom: 0;
+    margin-top: var(--spacer-2xl);
+  }
+  &__container {
+    margin: var(--spacer-sm);
+    @include for-desktop {
+      max-width: 69rem;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
