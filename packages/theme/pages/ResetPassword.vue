fbce53340c7ae13fc50ff0e3698faf5c716544ce
letelete <brunokawka@gmail.com>
2022-04-22 14:02:18 +0200

    fix: replace ~ import with @package

--- packages/theme/pages/ResetPassword.vue
+++ packages/theme/pages/ResetPassword.vue
@@ -66,7 +66,7 @@
 
 import { SfModal, SfButton, SfLoader, SfBar, SfInput } from '@storefront-ui/vue';
 import { ref, computed, useRoute } from '@nuxtjs/composition-api';
-import { useForgotPassword, forgotPasswordGetters } from '~/composables';
+import { useForgotPassword, forgotPasswordGetters } from '@vue-storefront/spree';
 import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
 import { required } from 'vee-validate/dist/rules';
 
